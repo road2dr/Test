78000c470bb661236666f87d34c8f9d3dbe87605
wasakei <wasakei@naver.com>
2017-02-24 10:40:10 +0900

    바꿈

--- app/src/main/java/com/mjict/test/testproject/LoginActivity.java
+++ app/src/main/java/com/mjict/test/testproject/LoginActivity.java
@@ -82,6 +82,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
+
+        //////// test
+
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
